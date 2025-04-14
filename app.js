7252aa883e81d9527f7531d1e3d82586ad685664
Thebeautifulmind <88392141+Aditya98run@users.noreply.github.com>
2025-04-14 22:02:09 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const followers = document.getElementById('followers');
-  const engagement = document.getElementById('engagement');
-  followers.textContent = '1,204';
-  engagement.textContent = '4.2%';
-});
